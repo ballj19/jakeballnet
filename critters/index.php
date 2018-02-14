9b741d94defa4a75fe4629d23a01957852da243c
Ball <Jacob.Ball@Nidec-mce.com>
2018-02-14 08:35:33 -0800

    styling

--- critters/index.php
+++ critters/index.php
@@ -52,11 +52,13 @@ echo '<div class="col-xs-12 critter-table">';
 
 foreach($result as $row)
 {
+	echo '<div class="critter-row">';
 	echo '<div class="col-xs-6 col-lg-2 critter-pic">test</div>';
 	echo '<div class="col-xs-6 col-lg-4 critter-name">' . $row['name'] . '</div>';
 	echo '<div class="col-xs-6 col-lg-3 critter-temp">' . $row['temperature'] . '&#8457</div>';
 	echo '<div class="col-xs-6 col-lg-3 critter-hum">' . $row['humidity'] . '%</div>';
-	
+	echo '</div>';
+
 	if(isset($_GET['text'])) // This is to keep the text length short enough for verizon to send
 	{
 		$text = $text . "\n";
