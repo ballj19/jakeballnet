0a828e2eef0ce2c5156ea71d7b8baa6b0a98ccf5
Jake Ball <ballj19@gmail.com>
2018-06-15 15:22:17 -0700

    more hwr changes

--- HogWildReptiles/index.php
+++ HogWildReptiles/index.php
@@ -1,12 +1,12 @@
 <html>
 <head>
+        <link rel="stylesheet" href="style.css">
+        <script src="javascript.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-        <script src="javascript.js"></script>
 </head>
 <body>
         <div class="banner-container">
@@ -33,8 +33,43 @@
                 <div class="banner-text">Hog Wild Reptiles</div>
                 <div id="explore-btn" class="btn">Explore</div>
         </div>
-        <div id="explore-container">
+        <div id="explore-container" class="row">
+        <?php
+                $for_sale = scandir('ForSale/');
+                for($i = 2; $i < sizeof($for_sale); $i++)
+                {
+                        echo '<div class="reptile col-md-8 offset-md-2">';
+                        echo '<img class="reptile-img1" src="ForSale/' . $for_sale[$i] . '/img1.jpg"/>';
+                 ?>       
+                        <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+    <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+    <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
+    <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>
+  </ul>
 
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+      <h3>HOME</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>Menu 1</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Menu 3</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
+        <?php        
+                        echo '</div>';
+                }
+        ?>
         </div>
 </body>
 </html>
