adf8eb1540e3abcd7b2c5df15a32c8453dc8da9d
Jake Ball <ballj19@gmail.com>
2018-07-31 15:20:23 -0700

    added functions.php file

---
+++ HogWildReptiles/functions.php
@@ -0,0 +1,27 @@
+<?php
+        function Database_Connect($database)
+        {
+                $servername = "localhost";
+                if($_SERVER['HTTP_HOST'] == 'localhost')
+                {
+                        $username = "root";
+                        $password = "";
+                        $dbname = $database;
+                }
+                else 
+                {
+                        $username = "ballj19_root";
+                        $password = "sitkbm19";
+                        $dbname = 'ballj19_' . $database;
+                }
+
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
+
+                return $conn;
+        }
+?>
